1a870dc74208127ca59da32556cba0a0fe73eb2b
Sergioesc28-arte <Sergiopro128@gmail.com>
2024-11-29 02:45:17 -0600

    añadir nueva ruta

--- controllers/Controllers.js
+++ controllers/Controllers.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { Usuario, Mascota } = require('../models/models.js'); // Asegúrate de importar tus modelos de Mongoose
+const { Usuario, Mascota,Encuentro, Match,Notificacion, Solicitud } = require('../models/models.js'); // Asegúrate de importar tus modelos de Mongoose
 
 // Login
 exports.login = async (req, res) => {
@@ -197,5 +197,240 @@ exports.deleteMascota = async (req, res) => {
         res.json(deletedMascota);
     } catch (error) {
         res.status(500).json({ message: 'Error al eliminar la mascota', error });
+    }
+};
+
+// -------- CRUD para Encuentros --------
+exports.getAllEncuentros = async (req, res) => {
+    try {
+        const encuentros = await Encuentro.find();
+        res.json(encuentros);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los encuentros', error });
+    }
+};
+
+exports.getEncuentroById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const encuentro = await Encuentro.findById(id);
+        if (!encuentro) {
+            return res.status(404).json({ message: 'Encuentro no encontrado' });
+        }
+        res.json(encuentro);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener el encuentro', error });
+    }
+};
+
+exports.createEncuentro = async (req, res) => {
+    try {
+        const nuevoEncuentro = new Encuentro(req.body);
+        await nuevoEncuentro.save();
+        res.status(201).json(nuevoEncuentro);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el encuentro', error });
+    }
+};
+
+exports.updateEncuentro = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const encuentroActualizado = await Encuentro.findByIdAndUpdate(id, req.body, { new: true });
+        if (!encuentroActualizado) {
+            return res.status(404).json({ message: 'Encuentro no encontrado' });
+        }
+        res.json(encuentroActualizado);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar el encuentro', error });
+    }
+};
+
+exports.deleteEncuentro = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const encuentroEliminado = await Encuentro.findByIdAndDelete(id);
+        if (!encuentroEliminado) {
+            return res.status(404).json({ message: 'Encuentro no encontrado' });
+        }
+        res.json({ message: 'Encuentro eliminado' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar el encuentro', error });
+    }
+};
+
+// -------- CRUD para Matches --------
+exports.getAllMatches = async (req, res) => {
+    try {
+        const matches = await Match.find();
+        res.json(matches);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los matches', error });
+    }
+};
+
+exports.getMatchById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const match = await Match.findById(id);
+        if (!match) {
+            return res.status(404).json({ message: 'Match no encontrado' });
+        }
+        res.json(match);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener el match', error });
+    }
+};
+
+exports.createMatch = async (req, res) => {
+    try {
+        const nuevoMatch = new Match(req.body);
+        await nuevoMatch.save();
+        res.status(201).json(nuevoMatch);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el match', error });
+    }
+};
+
+exports.updateMatch = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const matchActualizado = await Match.findByIdAndUpdate(id, req.body, { new: true });
+        if (!matchActualizado) {
+            return res.status(404).json({ message: 'Match no encontrado' });
+        }
+        res.json(matchActualizado);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar el match', error });
+    }
+};
+
+exports.deleteMatch = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const matchEliminado = await Match.findByIdAndDelete(id);
+        if (!matchEliminado) {
+            return res.status(404).json({ message: 'Match no encontrado' });
+        }
+        res.json({ message: 'Match eliminado' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar el match', error });
+    }
+};
+
+//CRUD MASCOTAS
+exports.getAllNotificaciones = async (req, res) => {
+    try {
+        const notificaciones = await Notificacion.find();
+        res.json(notificaciones);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener las notificaciones', error });
+    }
+};
+
+exports.getNotificacionById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const notificacion = await Notificacion.findById(id);
+        if (!notificacion) {
+            return res.status(404).json({ message: 'Notificación no encontrada' });
+        }
+        res.json(notificacion);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener la notificación', error });
+    }
+};
+
+exports.createNotificacion = async (req, res) => {
+    try {
+        const nuevaNotificacion = new Notificacion(req.body);
+        await nuevaNotificacion.save();
+        res.status(201).json(nuevaNotificacion);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear la notificación', error });
+    }
+};
+
+exports.updateNotificacion = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const notificacionActualizada = await Notificacion.findByIdAndUpdate(id, req.body, { new: true });
+        if (!notificacionActualizada) {
+            return res.status(404).json({ message: 'Notificación no encontrada' });
+        }
+        res.json(notificacionActualizada);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la notificación', error });
+    }
+};
+
+exports.deleteNotificacion = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const notificacionEliminada = await Notificacion.findByIdAndDelete(id);
+        if (!notificacionEliminada) {
+            return res.status(404).json({ message: 'Notificación no encontrada' });
+        }
+        res.json({ message: 'Notificación eliminada' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la notificación', error });
+    }
+};
+//CRUD SOLICITUDES
+exports.getAllSolicitudes = async (req, res) => {
+    try {
+        const solicitudes = await Solicitud.find();
+        res.json(solicitudes);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener las solicitudes', error });
+    }
+};
+
+exports.getSolicitudById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const solicitud = await Solicitud.findById(id);
+        if (!solicitud) {
+            return res.status(404).json({ message: 'Solicitud no encontrada' });
+        }
+        res.json(solicitud);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener la solicitud', error });
+    }
+};
+
+exports.createSolicitud = async (req, res) => {
+    try {
+        const nuevaSolicitud = new Solicitud(req.body);
+        await nuevaSolicitud.save();
+        res.status(201).json(nuevaSolicitud);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear la solicitud', error });
+    }
+};
+
+exports.updateSolicitud = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const solicitudActualizada = await Solicitud.findByIdAndUpdate(id, req.body, { new: true });
+        if (!solicitudActualizada) {
+            return res.status(404).json({ message: 'Solicitud no encontrada' });
+        }
+        res.json(solicitudActualizada);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la solicitud', error });
+    }
+};
+
+exports.deleteSolicitud = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const solicitudEliminada = await Solicitud.findByIdAndDelete(id);
+        if (!solicitudEliminada) {
+            return res.status(404).json({ message: 'Solicitud no encontrada' });
+        }
+        res.json({ message: 'Solicitud eliminada' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la solicitud', error });
     }
 };
