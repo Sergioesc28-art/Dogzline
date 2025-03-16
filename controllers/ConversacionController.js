711fc90e965ee11fae226987df5d52e0afd4f9f0
Jesus Ledesma <149735335+Jelex16@users.noreply.github.com>
2025-03-16 16:36:42 -0600

    new change

--- controllers/ConversacionController.js
+++ controllers/ConversacionController.js
@@ -1,19 +1,24 @@
 // Archivo de configuración de socket en el cliente
-import io from 'socket.io-client';
+const io = require('socket.io-client');
 
-const ENDPOINT = 'https://tu-app-en-render.com';
+const ENDPOINT = 'https://dogzline-j7t6.onrender.com';
 const socket = io(ENDPOINT);
 
-export const joinChat = (conversacionId) => {
+const joinChat = (conversacionId) => {
   socket.emit('join_chat', conversacionId);
 };
 
-export const sendMessage = (messageData) => {
+const sendMessage = (messageData) => {
   socket.emit('send_message', messageData);
 };
 
-export const receiveMessage = (callback) => {
+const receiveMessage = (callback) => {
   socket.on('receive_message', callback);
 };
 
-export default socket;
+module.exports = {
+  socket,
+  joinChat,
+  sendMessage,
+  receiveMessage
+};
